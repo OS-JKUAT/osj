c4b623723460cb7055a4b8895c235b883ea76dda
lewys-miugo <lewiswambugu01@gmail.com>
2024-10-01 08:07:30 +0300

    initial commit/file uploads

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Team;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        // Creating default roles
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+
+        // Seeding the admin user
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@osj.com',
+            'password' => Hash::make('password'), // hashed password
+        ]);
+
+        // Assigning role to admin
+        $admin->assignRole($adminRole);
+
+        // Seeding a regular user
+        $user = User::create([
+            'name' => 'Regular User',
+            'email' => 'user@osj.com',
+            'password' => Hash::make('password'), // hashed password
+        ]);
+
+        // Assigning role to user
+        $user->assignRole($userRole);
+    }
+}
