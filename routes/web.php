784ea4a3d74f50b27f0debd146656282bf5731c8
lewys-miugo <lewiswambugu01@gmail.com>
2025-01-07 08:07:39 +0300

    Adding the homepage,arrested, injured, missing and ud routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+use App\Livewire\Arrested\ArrestedHomePage;
 
+use App\Livewire\Injured\InjuredHomePage;
+
+use App\Livewire\Missing\MissingHomePage;
+
+use App\Livewire\Ud\UnidentifiedDecendentPage;
+
+use App\Livewire\HomePage;
+
+
+Route::get('/', HomePage::class)->name('home');
+
+// Arrested
+Route::get('arrested-home', ArrestedHomePage::class)
+    ->name('arrested');
+
+// Missing Page
+Route::get('missing-home', MissingHomePage::class)
+    ->name('missing');
+// Injured Page
+Route::get('injured-persons-home', InjuredHomePage::class)
+    ->name('injured');
+
+// UD
+Route::get('unidentified-decendents-home', UnidentifiedDecendentPage::class)
+    ->name('ud');
+
+// Auth
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -11,5 +38,7 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+
 
 require __DIR__.'/auth.php';
