31082ff7cd89c33c862e290f6db1e3cdc557682d
lewys-miugo <lewiswambugu01@gmail.com>
2025-01-07 07:38:08 +0300

    Footer

---
+++ resources/views/livewire/layout/footer.blade.php
@@ -0,0 +1,11 @@
+
+<footer class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <!-- Primary Footer -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <p class="text-center text-gray-600">&copy; 2025 Open Source JKUAT. All rights reserved. </p>
+        <a href="https://github.com/lewys-miugo" class="text-center text-gray-600">
+            <p>~ lewys ~</p>
+        </a>
+    </div>
+</footer>
+
