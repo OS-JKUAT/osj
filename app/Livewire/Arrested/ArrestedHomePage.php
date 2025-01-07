c6e80b76abb6bbf8a886a8ea8b45d002306df7de
lewys-miugo <lewiswambugu01@gmail.com>
2025-01-07 07:25:51 +0300

    Created arrested section pages

---
+++ app/Livewire/Arrested/ArrestedHomePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Arrested;
+
+use Livewire\Component;
+
+class ArrestedHomePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.arrested.arrested-home-page');
+    }
+}
