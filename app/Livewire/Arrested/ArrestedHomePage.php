95e1cc5d1e12f5925fabb7ab2297da987d6e294f
lewys-miugo <lewiswambugu01@gmail.com>
2025-01-07 08:05:52 +0300

    Adding the layouts-app.page

--- app/Livewire/Arrested/ArrestedHomePage.php
+++ app/Livewire/Arrested/ArrestedHomePage.php
@@ -8,6 +8,6 @@ class ArrestedHomePage extends Component
 {
     public function render()
     {
-        return view('livewire.arrested.arrested-home-page');
+        return view('livewire.arrested.arrested-home-page')->layout('layouts.app');
     }
 }
